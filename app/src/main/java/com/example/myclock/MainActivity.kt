5342fb65d238ba84ce473916c4efc0cadd1e0212
VyachAndreev <vyacheslavand@gmail.com>
2021-08-22 17:50:55 +0300

    hands added

--- app/src/main/java/com/example/myclock/MainActivity.kt
+++ app/src/main/java/com/example/myclock/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myclock
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import com.example.myclock.view.MyClock
+import timber.log.Timber
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var clock: MyClock
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clock = findViewById(R.id.clock)
+        Timber.i("setting time")
+        clock.setTime(Calendar.getInstance())
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
     }
 }
--- app/src/main/java/com/example/myclock/view/MyClock.kt
+++ app/src/main/java/com/example/myclock/view/MyClock.kt
@@ -6,53 +6,59 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import android.widget.RelativeLayout
+import androidx.annotation.ColorInt
 import com.example.myclock.R
 import timber.log.Timber
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
+import java.util.*
+import kotlin.concurrent.thread
+import kotlin.coroutines.CoroutineContext
+import kotlin.math.*
 
-class MyClock(context: Context, attrs: AttributeSet, ): View(context, attrs)
-{
+class MyClock(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
     private val arr = context.obtainStyledAttributes(attrs, R.styleable.MyClock)
     private val strokeWidth = arr.getDimension(R.styleable.MyClock_strokeWidth, 2f)
 
     private val n = 0.1f
+    private var updating = false
 
     private var cx = 0f
     private var cy = 0f
     private var radius = 0f
+    private var hourHandScaler = 0.5f
+    private var minuteHandScaler = 0.7f
+    private var secondHandScaler = 0.85f
 
-    private var paint: Paint = Paint().apply {
+    private lateinit var calendar: Calendar
+    private var mCanvas: Canvas? = null
+
+    private val paint: Paint = Paint().apply {
         color = Color.BLACK
         style = Paint.Style.STROKE
-        strokeWidth = this@MyClock.strokeWidth.toFloat()
+        strokeWidth = this@MyClock.strokeWidth
+    }
+
+    private val handPaint = Paint().apply {
+        color = Color.BLACK
+        style = Paint.Style.FILL
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        Timber.i("""width: $widthMeasureSpec
-            |height: $heightMeasureSpec
-        """.trimMargin())
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        Timber.i("OnLayout")
+        cx = (left + right).toFloat() / 2
+        cy = (top + bottom).toFloat() / 2
+        radius = minOf(bottom - top, right - left).toFloat() / 2 - strokeWidth
         super.onLayout(changed, left, top, right, bottom)
-        Timber.i(
-            """left: $left
-                |top: $top
-                |right: $right
-                |bottom: $bottom
-            """.trimMargin()
-        )
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        mCanvas = canvas
         canvas?.let {
-            cx = (left + right).toFloat() / 2
-            cy = (top + bottom).toFloat() / 2
-            radius = minOf(bottom - top, right - left).toFloat() / 2 - strokeWidth
             Timber.i(
                 """cx: $cx
                     |cy: $cy
@@ -69,10 +75,83 @@ class MyClock(context: Context, attrs: AttributeSet, ): View(context, attrs)
                     cy + sin(a) * radius * (1 - n),
                     paint,
                 )
-                Timber.i("sin(a) = ${sin(a)}")
-                a += (PI/6).toFloat()
-                Timber.i("a: $a")
+                a += (PI / 6).toFloat()
             }
+            it.drawCircle(cx, cy, 10f, paint.apply { style = Paint.Style.FILL })
+            updateTime()
         }
+    }
+
+    fun setTime(calendar: Calendar) {
+        this.calendar = calendar
+        var seconds = calendar.get(Calendar.SECOND) + calendar.get(Calendar.MINUTE) * 60
+        var angle = seconds.toDouble() / 3_600 * 2 * PI
+        mCanvas?.drawLine(
+            cx,
+            cy,
+            cx + radius * minuteHandScaler * sin(angle).toFloat(),
+            cy - radius * minuteHandScaler * cos(angle).toFloat(),
+            handPaint.apply {
+                strokeWidth = 10f
+                color = Color.BLACK
+            },
+        )
+        seconds += calendar.get(Calendar.HOUR) * 3_600
+        angle = seconds.toDouble() / 43_200 * 2 * PI
+        mCanvas?.drawLine(
+            cx,
+            cy,
+            cx + radius * hourHandScaler * sin(angle).toFloat(),
+            cy - radius * hourHandScaler * cos(angle).toFloat(),
+            handPaint.apply { strokeWidth = 20f },
+        )
+        seconds = calendar.get(Calendar.SECOND)
+        angle = seconds.toDouble() / 60 * 2 * PI
+        mCanvas?.drawLine(
+            cx - 20f * sin(angle).toFloat(),
+            cy + 20f * cos(angle).toFloat(),
+            cx + radius * secondHandScaler * sin(angle).toFloat(),
+            cy - radius * secondHandScaler * cos(angle).toFloat(),
+            handPaint.apply {
+                strokeWidth = 5f
+                color = Color.RED
+            },
+        )
+    }
+
+    fun getTime(): Calendar = calendar
+
+    fun updateTime() {
+        setTime(Calendar.getInstance())
+    }
+}
+
+class Hand(
+    width: Int,
+    length: Int,
+    @ColorInt handColor: Int,
+    context: Context,
+) : View(context) {
+    constructor(context: Context) : this(1, 1, Color.BLACK, context)
+
+    private val paint = Paint().apply {
+        color = handColor
+        style = Paint.Style.FILL
+    }
+
+    init {
+        Timber.i("length: $length")
+        layoutParams = RelativeLayout.LayoutParams(width, length)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawRect(
+            left.toFloat(),
+            top.toFloat(),
+            right.toFloat(),
+            (bottom + top).toFloat() / 2,
+            paint,
+        )
     }
 }
